c20c26aea0ab672572a44c0800265e91a8b5d0c4
KCMbogo <kadilana04@gmail.com>
2025-05-01 00:46:45 +0300

    login automation project

--- projects/login_automation.py
+++ projects/login_automation.py
@@ -3,8 +3,6 @@ import time
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
