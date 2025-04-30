34482c889699beb198c87a623d8bd0b3696a5c47
KCMbogo <kadilana04@gmail.com>
2025-05-01 00:26:59 +0300

    Login automation project

---
+++ projects/login_automation.py
@@ -0,0 +1,51 @@
+import time
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+
+options = Options()
+
+options.add_argument("--start-maximized")
+options.add_argument("disable-blink-features=AutomationControlled")
+options.add_experimental_option("useAutomationExtension", False)
+
+driver = Chrome(options=options)
+
+try:
+    driver.get(url="https://the-internet.herokuapp.com/login")
+except Exception as e:
+    print(f"Failed to get url: {e}")
+
+time.sleep(2)
+
+try:
+    username_field = driver.find_element(by=By.NAME, value="username")
+    pwd_field = driver.find_element(by=By.NAME, value="password")
+    submit = driver.find_element(by=By.CSS_SELECTOR, value='button[type="submit"]')
+
+    if username_field or pwd_field:
+        username_field.send_keys("tomsmith")
+        pwd_field.send_keys("SuperSecretPassword!")
+
+        try:
+            submit.click()
+            time.sleep(1)
+            success = driver.find_element(by=By.CLASS_NAME, value="success")
+            if success:
+                print(f"Message: {success.text}")
+                current_url = driver.current_url
+                print(f"Current URL: {current_url}")
+                print(f"Webpage content:\n {driver.page_source}")
+
+                input("Press Enter to quit")
+        except:
+            driver.execute_script("arguments[0].click();", submit)
+
+except Exception as e:
+    print(f"Error: {e}")
+    driver.quit()
+
